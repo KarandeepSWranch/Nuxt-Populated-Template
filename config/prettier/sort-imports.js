355e9c8ee9ae531ea44376b3a368e74b1b6f5e9b
Karan <karandeepswranch@gmail.com>
2023-05-17 14:05:34 -0700

    chore(prettier): update sort-imports plugin to be more efficient

--- config/prettier/sort-imports.js
+++ config/prettier/sort-imports.js
@@ -12,19 +12,40 @@ module.exports = {
 };
 
 function sortImports(text) {
+    // Split the text into lines
     const lines = text.split('\n');
 
-    const importLines = lines.filter((line) => line.startsWith('import') && !line.startsWith('import {') && !line.startsWith('import type'));
-    const importModules = lines.filter((line) => line.startsWith('import {'));
-    const importTypes = lines.filter((line) => line.startsWith('import type'));
+    // Categorize lines into different types of imports and non-imports
+    const importLinesRegular = [];
+    const importLinesModules = [];
+    const importLinesTypes = [];
+    const nonImportLines = [];
 
-    const nonImportLines = lines.filter((line) => !line.startsWith('import'));
+    lines.forEach((line) => {
+        if (line.startsWith('import ')) {
+            if (line.startsWith('import {')) {
+                importLinesModules.push(line);
+            } else if (line.startsWith('import type')) {
+                importLinesTypes.push(line);
+            } else {
+                importLinesRegular.push(line);
+            }
+        } else {
+            nonImportLines.push(line);
+        }
+    });
 
-    const sortedImportLines = importLines.sort((a, b) => a.length - b.length);
-    const sortedImportModules = importModules.sort((a, b) => a.length - b.length);
-    const sortedImportTypes = importTypes.sort((a, b) => a.length - b.length);
+    // Function to sort lines based on their length
+    const sortByLength = (a, b) => a.length - b.length;
 
-    const sortedImports = [...sortedImportLines, '\n', ...sortedImportModules, '\n', ...sortedImportTypes];
+    // Sort each type of import line
+    const sortedRegularImports = importLinesRegular.sort(sortByLength);
+    const sortedModuleImports = importLinesModules.sort(sortByLength);
+    const sortedTypeImports = importLinesTypes.sort(sortByLength);
 
+    // Combine all imports, separating types with a new line
+    const sortedImports = [...sortedRegularImports, '\n', ...sortedModuleImports, '\n', ...sortedTypeImports];
+
+    // Combine sorted imports with non-import lines and join them into a single string
     return [...sortedImports, ...nonImportLines].join('\n');
 }
